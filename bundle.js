1422da72d7bf11cd026d5b7123d2f3511b095c4f
Sean Wallace <wallace.sean111@gmail.com>
2022-11-28 10:48:30 +0100

    Add and pass first tests and ShipFactory

---
+++ bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+ console.log('Hello world!')
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9iYXR0bGVzaGlwLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbIiBjb25zb2xlLmxvZygnSGVsbG8gd29ybGQhJylcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
